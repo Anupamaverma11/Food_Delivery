bea183b8d1163eb247cf3e4bfa48a19e741ca8ac
Anupama Verma <126940536+Anupamaverma11@users.noreply.github.com>
2024-10-30 23:05:14 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 export const connectDB=async()=>{
-   await mongoose.connect('mongodb+srv://greatstack:shresth@cluster0.ke7guyr.mongodb.net/food-del').
+   await mongoose.connect('mongodb+srv://anupamaverma2101:0S4vxbWjD7yAUFnz@cluster0.kjxbj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').
    then(()=>console.log("DB connected"))
 }
